9ee601f065783be1d8f8f5e30572aa2aa8445019
Yuri Chukhlib <Developer4@MBP-Yurii.routerf77ebc.com>
2017-03-26 12:54:31 +0300

    Login UI Integration
    SwiftyVK imported.
    MBProgressHUD imported.
    Login controllers added.

---
+++ VK-API-Test-Project/VK-API-Test-Project/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  VK-API-Test-Project
+//
+//  Created by Yurii Chukhlib on 26.03.17.
+//  Copyright © 2017 Yuri Chukhlib. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	@IBAction func loginButtonAction(_ sender: Any) {
+	}
+}
